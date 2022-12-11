01f1aa09795a2397c15ab2569b853c4cbfb81066
Jagreet <jagreetdg@gmail.com>
2022-12-11 18:15:24 +0530

    Added Dependencies & Structured Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import MainForm from './containers/RegistrationForm';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { ReactNotifications } from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ReactNotifications/>
+      <MainForm/>
     </div>
   );
 }
